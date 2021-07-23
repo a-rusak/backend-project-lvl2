0e4f28ef16b13749c13418965ab190706090b5b1
Aleksandr Rusak <arusak@ourgapps.com>
2021-07-23 16:46:49 +0300

    #6: WIP Format deep objects

--- src/formatter.js
+++ src/formatter.js
@@ -1,10 +1,23 @@
-const { compareMapping } = require('./compareMapping');
+const {
+  compareMapping: {
+    IN_SECOND_ONLY, EQUAL, IN_FIRST_ONLY, DIFF_VALUE, DIFF_VALUE_SECOND,
+  },
+} = require('./compareMapping');
 
 exports.formatToString = (arr) => {
-  const diffStr = arr.reduce((acc, [key, { value, state }]) => {
-    const { sign } = Object.values(compareMapping).find(({ name }) => name === state);
-    return `${acc}
-  ${sign} ${key}: ${value}`;
-  }, '');
-  return `{${diffStr}\n}`;
+  const signMapping = {
+    [IN_SECOND_ONLY]: '+',
+    [EQUAL]: ' ',
+    [IN_FIRST_ONLY]: '-',
+    [DIFF_VALUE]: '-',
+    [DIFF_VALUE_SECOND]: '+',
+  };
+  const strings = arr.map(([key, { $body, $type, $depth }], idx) => {
+    const lastDepth = arr[idx - (idx > 0 ? 1 : 0)][1].$depth;
+    const indent = ' '.repeat($depth * 4 - 2) + ($type ? `${signMapping[$type]} ` : '  ');
+    const closeBrackets = lastDepth > $depth ? `${'.'.repeat($depth * 4)}}\n`.repeat(lastDepth - $depth) : '';
+    // ' '.repeat($depth * 4)
+    return `${closeBrackets}${indent}${key}: ${$body === undefined ? '{' : $body}\n`;
+  }, []);
+  return `{\n${strings.join('')}\n}`;
 };
