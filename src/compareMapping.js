8b8556021378a7579f31ae1899e876c5e417c33e
Aleksandr Rusak <arusak@ourgapps.com>
2021-07-23 16:13:30 +0300

    #6: Compare deep objects

--- src/compareMapping.js
+++ src/compareMapping.js
@@ -1,22 +1,7 @@
 exports.compareMapping = {
-  EQUAL: {
-    name: 'equal',
-    sign: ' ',
-  },
-  IN_FIRST_ONLY: {
-    name: 'inFirstOnly',
-    sign: '-',
-  },
-  IN_SECOND_ONLY: {
-    name: 'inSecondOnly',
-    sign: '+',
-  },
-  DIFF_VALUE_FIRST: {
-    name: 'diffValueFirst',
-    sign: '-',
-  },
-  DIFF_VALUE_SECOND: {
-    name: 'diffValueSecond',
-    sign: '+',
-  },
+  EQUAL: 'equal',
+  IN_FIRST_ONLY: 'inFirstOnly',
+  IN_SECOND_ONLY: 'inSecondOnly',
+  DIFF_VALUE: 'diffValue',
+  DIFF_VALUE_SECOND: 'diffValueSecond',
 };
--- src/diff.js
+++ src/diff.js
@@ -1,43 +1,170 @@
 const _ = require('lodash');
 
-const { compareMapping } = require('./compareMapping');
+const {
+  compareMapping: {
+    IN_SECOND_ONLY, EQUAL, IN_FIRST_ONLY, DIFF_VALUE, DIFF_VALUE_SECOND,
+  },
+} = require('./compareMapping');
+
+const isPrimitive = (v) => typeof v === 'string'
+  || typeof v === 'number'
+  || typeof v === 'boolean'
+  || v === null;
 
 const prepareObj = (obj) => Object
   .entries(obj)
   .reduce((acc, [key, value]) => ({
     ...acc,
-    [key]: { value, state: null },
+    [key]: {
+      $body: isPrimitive(value) ? value : prepareObj(value),
+      $type: null,
+    },
   }), {});
 
-exports.getDiff = (obj1, obj2) => {
-  const n1 = prepareObj(obj1);
-  const n2 = prepareObj(obj2);
-  Object.entries(n1).forEach(([key, va]) => {
-    const v = va;
-    if (n2[key] === undefined) {
-      v.state = compareMapping.IN_FIRST_ONLY.name;
-    } else if (n2[key].value === v.value) {
-      v.state = compareMapping.EQUAL.name;
-      n2[key].state = compareMapping.EQUAL.name;
+exports.getDiff = (o1, o2) => {
+  const n1 = prepareObj(o1);
+  const n2 = prepareObj(o2);
+  // console.log(JSON.stringify(n2, null, 2));
+
+  const iter1 = (obj1, obj2) => {
+    Object.entries(obj1).forEach(([key, v]) => {
+      const body1 = v.$body;
+      Object.entries(obj2).forEach(([key2, v2]) => {
+        if (!Object.keys(obj1).includes(key2)) {
+          v2.$type = IN_SECOND_ONLY;
+          obj1[key2] = v2;
+        }
+      });
+      if (isPrimitive(body1)) {
+        // console.log(key, value);
+        if (obj2[key] === undefined) {
+          v.$type = IN_FIRST_ONLY;
+        } else if (obj2[key].$body === body1) {
+          v.$type = EQUAL;
+        } else {
+          v.$type = DIFF_VALUE;
+          v.$bodySecond = obj2[key].$body;
+        }
+      } else if (obj2[key] === undefined) {
+        v.$type = IN_FIRST_ONLY;
+      } else {
+        const body2 = obj2[key].$body;
+        if (isPrimitive(body2)) {
+          v.$type = DIFF_VALUE;
+          v.$bodySecond = obj2[key].$body;
+        } else {
+          iter1(body1, body2);
+        }
+      }
+    });
+  };
+  iter1(n1, n2);
+
+  // console.log(JSON.stringify(n1, null, 2));
+
+  const addDepth = (arr, depth = 1) => {
+    arr.forEach((value) => {
+      value.$depth = depth;
+      if (!isPrimitive(value.$body)) {
+        addDepth(Object.values(value.$body), depth + 1);
+      }
+    });
+  };
+  addDepth(Object.values(n1));
+
+  // console.log(JSON.stringify(n1, null, 2));
+
+  const toEntriesDeep = (arr) => arr.map(([key, value]) => {
+    if (!isPrimitive(value.$body)) {
+      return [key, {
+        ...value,
+        $body: toEntriesDeep(Object.entries(value.$body)),
+      }];
+    }
+    return [key, value];
+  });
+
+  const entries = toEntriesDeep(Object.entries(n1));
+
+  const sortDeep = (arr) => {
+    arr.sort((a, b) => a[0] > b[0]);
+    arr.forEach(([, value]) => {
+      if (!isPrimitive(value.$body)) {
+        sortDeep(value.$body);
+      }
+    });
+  };
+
+  sortDeep(entries);
+
+  // console.log(JSON.stringify(entries, null, 2));
+
+  const flatDeep = (arr) => arr.reduce((acc, [key, value]) => {
+    // FIXME: missed $bodySecond on group1/nest key due to value.$body is an Array
+    // if (isPrimitive(value.$body)) {
+    //   const records = [[key, value]];
+    //   if (value.$bodySecond !== undefined) {
+    //     records.push([key, {
+    //       $type: DIFF_VALUE_SECOND,
+    //       $depth: value.$depth,
+    //       $body: value.$bodySecond,
+    //     }]);
+    //   }
+    //   return [
+    //     ...acc,
+    //     ...records,
+    //   ];
+    // }
+    // return [
+    //   ...acc,
+    //   [key, {
+    //     $type: value.$type,
+    //     $depth: value.$depth,
+    //   }],
+    //   ...flatDeep(value.$body),
+    // ];
+    const records = [];
+    const recordsToFlat = [];
+    if (isPrimitive(value.$body)) {
+      records.push([key, {
+        $type: value.$type,
+        $depth: value.$depth,
+        $body: value.$body,
+      }]);
     } else {
-      v.state = compareMapping.DIFF_VALUE_FIRST.name;
-      n2[key].state = compareMapping.DIFF_VALUE_SECOND.name;
+      recordsToFlat.push([key, {
+        $type: value.$type,
+        $depth: value.$depth,
+      }],
+      ...flatDeep(value.$body));
     }
-  });
-  const diffArr = Object.entries(n1);
-  Object.entries(n2).forEach(([key, v]) => {
-    if (v.state === null) {
-      diffArr.push([key, {
-        value: v.value,
-        state: compareMapping.IN_SECOND_ONLY.name,
-      }]);
+    if (value.$bodySecond !== undefined) {
+      if (isPrimitive(value.$bodySecond)) {
+        console.log(key, value.$bodySecond);
+        records.push([key, {
+          $type: DIFF_VALUE_SECOND,
+          $depth: value.$depth,
+          $body: value.$bodySecond,
+        }]);
+      } else {
+        console.log('!!!!!');
+        recordsToFlat.push([key, {
+          $type: value.$type,
+          $depth: value.$depth,
+        }],
+        ...flatDeep(value.$bodySecond));
+      }
     }
-    if (v.state === compareMapping.DIFF_VALUE_SECOND.name) {
-      diffArr.push([key, {
-        value: v.value,
-        state: compareMapping.DIFF_VALUE_SECOND.name,
-      }]);
-    }
-  });
-  return _.sortBy(diffArr, [0]);
+    return [
+      ...acc,
+      ...recordsToFlat,
+      ...records,
+    ];
+  }, []);
+
+  const flatEntries = flatDeep(entries);
+
+  // console.log(JSON.stringify(flatEntries, null, 2));
+
+  return flatEntries;
 };
