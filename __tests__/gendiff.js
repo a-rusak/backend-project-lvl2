21889c4a5d6f7b87a1fe444380be4d7fc9242e59
Aleksandr Rusak <arusak@ourgapps.com>
2021-08-02 17:16:51 +0300

    #6: Fix diff fixture, tests

--- __tests__/gendiff.js
+++ __tests__/gendiff.js
@@ -1,16 +1,15 @@
 const { test, expect } = require('@jest/globals');
 
+const fs = require('fs');
+const path = require('path');
 const { getFormattedDiff } = require('../index');
 
 test('Show diff for two json files with flat structure', () => {
-  expect(getFormattedDiff('file1.json', 'file2.json')).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+  const diff = fs
+    .readFileSync(path.resolve('__fixtures', 'files3-4.diff.txt'))
+    .toString('utf8');
+  console.log(diff);
+  expect(getFormattedDiff('file3.json', 'file4.json')).toEqual(diff);
 });
 
 test('Show diff for two yaml files with flat structure', () => {
